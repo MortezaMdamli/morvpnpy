8f8871bb1108473fdac4c9195789fb3900c1ab77
MortezaMdamli <81289733+MortezaMdamli@users.noreply.github.com>
2026-06-24 01:26:43 +0330

    Create subSC.py

---
+++ subSC.py
@@ -0,0 +1,40 @@
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/mix/sub.html
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/vless.html
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/vmess.html
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/trojan.html
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/ss.html
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/hy2.html
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/AT 🇦🇹.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/AU 🇦🇺.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/BE 🇧🇪.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/CA 🇨🇦.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/CH 🇨🇭.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/CL 🇨🇱.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/CO 🇨🇴.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/DE 🇩🇪.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/ES 🇪🇸.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/FI 🇫🇮.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/FR 🇫🇷.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/HK 🇭🇰.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/HU 🇭🇺.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/GB 🇬🇧.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/IL 🇮🇱.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/IE 🇮🇪.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/IN 🇮🇳.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/IL 🇮🇱.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/IT 🇮🇹.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/FI 🇫🇮.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/FR 🇫🇷.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/MX 🇲🇽.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/NL 🇳🇱.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/PH 🇵🇭.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/PL 🇵🇱.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/RO 🇷🇴.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/SK 🇸🇰.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/RU 🇷🇺.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/TH 🇹🇭.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/TR 🇹🇷.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/TW 🇹🇼.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/US 🇺🇸.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/XX ❓.txt
+https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/loc/ZA 🇿🇦.txt
